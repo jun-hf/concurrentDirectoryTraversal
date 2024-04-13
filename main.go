6a7b80cde9ff58d520057356f6bbc79377681289
jun-hf <fong.junh@gmail.com>
2024-04-13 12:41:31 +0100

    fixed program exiting before calling wg.Add() in a go routine

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -14,39 +17,64 @@ func main() {
 		roots = []string{"."}
 	}
 	
-	var wg sync.WaitGroup
 	fileSize := make(chan int64)
+	var wg sync.WaitGroup
 
-	for _, r := range roots{
-		wg.Add(1)
-		go TraverseDirectory(r, &wg, fileSize)
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, r := range roots {
+			wg.Add(1)
+			go TraverseDirectory(r, &wg, fileSize)
+		}
+	}()
 
 	go func() {
 		wg.Wait()
 		close(fileSize)
 	}()
 	
-
-	for a := range fileSize {
-		fmt.Println(a)
+	ticker := time.NewTicker(50 *time.Millisecond)
+	var totalBytes int64
+	var totalFiles int
+	loop:
+	for {
+		select {
+		case size, ok := <- fileSize:
+			if !ok {
+				break loop
+			}
+			totalBytes += size
+			totalFiles++
+		case <- ticker.C:
+			fmt.Printf("number of files: %v, number of bytes: %v\n", totalFiles, totalBytes)
+		}
 	}
+
+	fmt.Printf("number of files: %v, number of bytes: %v\n", totalFiles, totalBytes)
 }
 
 func TraverseDirectory(dir string, wg *sync.WaitGroup, fileSize chan <-int64) {
 	defer wg.Done()
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, f := range entries {
-		if !f.IsDir() {
+	for _, f := range ListDirectory(dir) {
+		if f.IsDir() {
+			dirPath := filepath.Join(dir, f.Name())
+			wg.Add(1)
+			TraverseDirectory(dirPath, wg, fileSize)
+		} else {
 			fileInfo, err := f.Info()
 			if err != nil {
 				fmt.Println(err)
 			}
-			fmt.Printf("adding file: %v\n", f.Name())
 			fileSize <- fileInfo.Size()
 		}
 	}
+}
+
+func ListDirectory(dir string) []fs.DirEntry {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return entries
 }
