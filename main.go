4e7bdf07279c0076c821dfed6b3e79890994a2a7
jun-hf <fong.junh@gmail.com>
2024-04-13 16:16:16 +0100

    add cancellation for traversal directory

--- main.go
+++ main.go
@@ -15,12 +15,27 @@ type rootSize struct {
 	byteSize int64
 }
 
+var done = make(chan bool)
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
+
 func main() {
 	flag.Parse()
 	roots := flag.Args()
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
 
 	rootCh := make(chan rootSize)
 	var wg sync.WaitGroup
@@ -53,6 +68,11 @@ loop:
 			totalFiles++
 		case <-ticker.C:
 			printRootContent(rootContent)
+		case <-done:
+			// drain the chanel, before exiting the loop
+			for range rootCh {
+			}
+			return
 		}
 	}
 
@@ -71,11 +91,14 @@ func printRootContent(rootList []rootSize) {
 
 func TraverseDirectory(dir string, wg *sync.WaitGroup, rootResponse chan<- rootSize, currentRoot string) {
 	defer wg.Done()
+	if cancelled() {
+		return
+	}
 	for _, f := range ListDirectory(dir) {
 		if f.IsDir() {
 			dirPath := filepath.Join(dir, f.Name())
 			wg.Add(1)
-			TraverseDirectory(dirPath, wg, rootResponse, currentRoot)
+			go TraverseDirectory(dirPath, wg, rootResponse, currentRoot)
 		} else {
 			fileInfo, err := f.Info()
 			if err != nil {
@@ -86,7 +109,15 @@ func TraverseDirectory(dir string, wg *sync.WaitGroup, rootResponse chan<- rootS
 	}
 }
 
+// sema is a semaphore that limits ListDirectory to 20 
+var sema = make(chan struct{}, 20)
+
 func ListDirectory(dir string) []fs.DirEntry {
+	select {
+	case sema <- struct{}{}:
+	case <-done:
+		return nil
+	}
 	entries, err := os.ReadDir(dir)
 	if err != nil {
 		fmt.Println(err)
